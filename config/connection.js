e9b1a64b13d56bbf5e050ef765841fe7a02effd0
Ben Camarillo <jtby97@gmail.com>
2021-11-15 19:29:18 -0600

    attempt at launch

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+  process.env.CLEARDB_DATABASE_URL
+)
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
